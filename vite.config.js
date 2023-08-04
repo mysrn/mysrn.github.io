2c3725fa7f94f2e4b458e894862f6ec12242381a
mysrn <yusron.dev@pwi.co.id>
2023-08-04 07:42:06 +0700

    push

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/mysrn.github.io/test-fe-agile-technica/',
-  build: {
-    outDir: 'dist', 
-  },
+export default defineConfig(({command}) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+    build: {
+      outDir: 'dist', 
+    }
+  }
+
+  if(command !== 'serve') 
+    config.base = '/mysrn.github.io/test-fe-agile-technica/'
+
+  return config
+  
 })
