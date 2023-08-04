a801e6269e49df4aac1c1b9f7f9731324255fb61
mysrn <yusron.dev@pwi.co.id>
2023-08-04 07:21:24 +0700

    push

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mysrn.github.io/test-fe-agile-technica',
+  base: '/mysrn.github.io/test-fe-agile-technica/',
   build: {
     outDir: 'dist', 
   },
